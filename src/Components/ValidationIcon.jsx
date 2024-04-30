72c4573fbfdd9306bf270bbf738608e6486f73fd
Chris Armbruster <chris@armbrustermail.com>
2024-04-30 06:32:58 -0500

    Add responsive wrapping to integrity checker.

--- src/Components/ValidationIcon.jsx
+++ src/Components/ValidationIcon.jsx
@@ -9,24 +9,27 @@ import { BiCheckCircle, BiXCircle } from "react-icons/bi";
 
 const ValidationIcon = ({ isValid, label, errors = [] }) => {
   return (
-    <ListGroup.Item className="border-0">
-      <span className="me-3">
+    <ListGroup.Item className="border-0 d-flex align-items-start">
+      <div className="me-3">
         {isValid ? (
           <BiCheckCircle className="text-success" size={32} />
         ) : (
           <BiXCircle className="text-danger" size={32} />
         )}
-      </span>
-      <span className="fw-bold">{label}</span>
-      {errors.length > 0 && (
-        <Alert variant="danger" className="ms-4 mt-3">
-          {errors.map((error, index) => (
-            <p key={index} className="my-0">
-              Block {error.blockNumber} failed {error.errorType} validation test
-            </p>
-          ))}
-        </Alert>
-      )}
+      </div>
+      <div>
+        <span className="fw-bold">{label}</span>
+        {errors.length > 0 && (
+          <Alert variant="danger" className="ms-4 mt-3">
+            {errors.map((error, index) => (
+              <p key={index} className="my-0">
+                Block {error.blockNumber} failed {error.errorType} validation
+                test
+              </p>
+            ))}
+          </Alert>
+        )}
+      </div>
     </ListGroup.Item>
   );
 };
